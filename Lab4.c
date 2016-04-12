d2913d923f4d6ced6038dc2068b67b4ce6fb9148
Jiayu Wang <Jiayu Wang>
2016-04-12 15:39:43 -0500

    updated push buttons

--- Lab4.c
+++ Lab4.c
@@ -17,15 +17,22 @@
 // Main Program:
 //
 //*****************************************************************************
+void	PBSwInit(void);	
+
 int
 main(void)
 {	
 	//Section to initialize switches and displays
 	    //Initialize display
+	
 	    //Initialize Buttons
-
+	int PB1, PB2;
+	PBSwInit();
+	
+	
 	//Define variables
 	    //2D array 128 by 96
+	int grid[128][96];
 	    //x,y coords of CPU and Player
 	    //Current direction for both cpu and user (0-left, 1 -up, 2-right, 3 - down)
 
