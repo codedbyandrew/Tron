3e8d6f6f6ffb49a154623df68e7d725b7524d77f
Andrew Lundholm <andrewatmadison@gmail.com>
2016-04-12 15:26:20 -0500

    add variable definitions

--- Lab4.c
+++ Lab4.c
@@ -27,7 +27,13 @@ main(void)
 	//Define variables
 	    //2D array 128 by 96
 	    //x,y coords of CPU and Player
+	    int cpuX;
+	    int cpuY;
+	    int playerX;
+	    int player Y;
 	    //Current direction for both cpu and user (0-left, 1 -up, 2-right, 3 - down)
+	    int cpuDir;
+	    int playerDir;
 
 
     //Display title screen
